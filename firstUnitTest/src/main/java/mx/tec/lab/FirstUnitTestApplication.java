e48801e33653381e10e89745df8962d86b22b939
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2021-02-15 19:01:39 -0700

    Lab

---
+++ firstUnitTest/src/main/java/mx/tec/lab/FirstUnitTestApplication.java
@@ -0,0 +1,14 @@
+package mx.tec.lab;
+
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstUnitTestApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FirstUnitTestApplication.class, args);
+	}
+
+}
